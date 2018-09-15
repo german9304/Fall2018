659c7bb285b0262e371594abb17000ccb1994656
german9304 <31298672+german9304@users.noreply.github.com>
2018-09-15 14:01:40 -0700

    program2

--- CSCI551/program2/mm.c
+++ CSCI551/program2/mm.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>  
 
-void random_matrices(int size){
- printf("size: %d\n",size);
+void random_matrices(int size, int matrix[][size]){
+ //int iSecret, iGuess;
+    printf("size: %d\n",size);
+ //matrix[0][0] = 1;
+
 }
-void matrices(int size, int arr[][size]){
-	 for(int i = 0;i < size; i++){
-			for(int j = 0;j < size; j++){
-				scanf("%d",&arr[i][j]);
-			}
-		}
+void matrices(int size, int matrix[][size]){
+    for(int i = 0;i < size; i++){
+       for(int j = 0;j < size; j++){
+        scanf("%d",&matrix[i][j]);
+       }
+    }
+    printf("matrix: \n");
+    for(int i = 0;i < size; i++){
+        for(int j = 0;j < size; j++){
+            printf("%d",matrix[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 int main(int argc, char const *argv[])
@@ -18,24 +29,18 @@ int main(int argc, char const *argv[])
     char flag[50];
     scanf("%s",flag);
     if(!strcmp(flag,"R")){
-    	int size;
-    	scanf("%d",&size);
-    	random_matrices(size);
+    	int size = 4800;
+    	//scanf("%d",&size);
+        int matrix[size][size];
+        printf("row col %d\n",matrix[0][0]);
+        printf("size: %d\n",size );
+    	//random_matrices(size, matrix);
     }else if(!strcmp(flag,"I")){
-    	int size;
+    	int size = 0;
     	scanf("%d",&size);
     	printf("size: %d\n",size);
-    	int m = size;
-    	int n = size;
-    	int matrix_1[m][n];
+    	int matrix_1[size][size];
     	matrices(size,matrix_1);
-    	printf("matrix: \n");
-    	for(int i = 0;i < size; i++){
-    		for(int j = 0;j < size; j++){
-    			printf("%d",matrix_1[i][j]);
-    		}
-    		printf("\n");
-    	}
 
     }else{
        //printf("Flag else type: %s\n",flag);
