67e3222f6db123b95db9aa762191fbe1a5b39f45
german9304 <31298672+german9304@users.noreply.github.com>
2018-09-15 14:48:32 -0700

    generated matrix

--- CSCI551/program2/mm.c
+++ CSCI551/program2/mm.c
@@ -7,6 +7,17 @@ void random_matrices(int size, int matrix[][size]){
  //int iSecret, iGuess;
     printf("size: %d\n",size);
  //matrix[0][0] = 1;
+    for(int i =0; i < size; i++){
+        for(int j = 0; j< size; j++){
+            matrix[i][j] = j;
+        }
+    }
+    printf("GENERATED MATRIX: \n");
+     for(int i =0; i < size; i++){
+        for(int j = 0; j< size; j++){
+            printf("%d\n",matrix[i][j]);
+        }
+    }
 
 }
 void matrices(int size, int matrix[][size]){
@@ -24,17 +35,17 @@ void matrices(int size, int matrix[][size]){
     }
 }
 
-int main(int argc, char const *argv[])
+int main()
 {
     char flag[50];
     scanf("%s",flag);
     if(!strcmp(flag,"R")){
         int size;
     	scanf("%d",&size);
-       // int matrix[size][size];
+        int matrix[size][size];
         //printf("row col %d\n",matrix[0][0]);
         printf("size: %d\n",size );
-    	//random_matrices(size, matrix);
+    	random_matrices(size, matrix);
     }else if(!strcmp(flag,"I")){
     	int size = 0;
     	scanf("%d",&size);
