267d82e9cb1527cbd615f9a0ff936e432da8c51e
German Razo <grazo1@o244-20.csuchico.edu>
2018-09-15 17:40:06 -0700

    random matrix

--- CSCI551/program2/mm.c
+++ CSCI551/program2/mm.c
@@ -3,33 +3,71 @@
 #include <string.h>
 #include <time.h>  
 
-void random_matrices(int size, int matrix[][size]){
- //int iSecret, iGuess;
-    printf("size: %d\n",size);
- //matrix[0][0] = 1;
-    for(int i =0; i < size; i++){
-        for(int j = 0; j< size; j++){
-            matrix[i][j] = j;
+void generate_random_matrices(int size, int matrix_1[][size], int matrix_2[][size]){
+
+    srand (time(0));
+    //int res = 0;
+     printf("MATRIX_1 \n");
+    for(int i = 0; i < size; i++){
+        printf("Iteration: %d\n",i);
+        for(int j = 0; j < size; j++){
+              matrix_1[i][j] = rand() % (50 + 1 - -50) + -50;
+            //res = rand() % (50 + 1 - -50) + -50;
+            printf(" %d ",matrix_1[i][j]);
         }
+        printf("\n");
     }
-    printf("GENERATED MATRIX: \n");
-     for(int i =0; i < size; i++){
-        for(int j = 0; j< size; j++){
-            printf("%d\n",matrix[i][j]);
+    printf("MATRIX_2 \n");
+    for(int i = 0; i < size; i++){
+        printf("Iteration: %d\n",i);
+        for(int j = 0; j < size; j++){
+              matrix_2[i][j] = rand() % (50 + 1 - -50) + -50;
+             //res = rand() % (50 + 1 - -50) + -50;
+             printf(" %d ",matrix_2[i][j] );
         }
+        printf("\n");
     }
 
-}
-void matrices(int size, int matrix[][size]){
-    for(int i = 0;i < size; i++){
-       for(int j = 0;j < size; j++){
-        scanf("%d",&matrix[i][j]);
-       }
-    }
-    printf("matrix: \n");
+ //matrix[0][0] = 1;
+    /*
+     printf("matrix_1: \n");
     for(int i = 0;i < size; i++){
         for(int j = 0;j < size; j++){
-            printf("%d",matrix[i][j]);
+            printf("%d",matrix_1[i][j]);
+        }
+        printf("\n");
+    }
+     printf("matrix_2: \n");
+    for(int i = 0;i < size; i++){
+        for(int j = 0;j < size; j++){
+            printf("%d",matrix_2[i][j]);
+        }
+        printf("\n");
+    }
+    */
+}
+void generate_matrices(int size, int matrix_1[][size], int matrix_2[][size]){
+    for(int i = 0;i < size; i++){
+       for(int j = 0;j < size; j++){
+        scanf("%d",&matrix_1[i][j]);
+       }
+    }
+    for(int i = 0;i < size; i++){
+       for(int j = 0;j < size; j++){
+        scanf("%d",&matrix_2[i][j]);
+       }
+    }
+    printf("matrix_1: \n");
+    for(int i = 0;i < size; i++){
+        for(int j = 0;j < size; j++){
+            printf("%d",matrix_1[i][j]);
+        }
+        printf("\n");
+    }
+     printf("matrix_2: \n");
+    for(int i = 0;i < size; i++){
+        for(int j = 0;j < size; j++){
+            printf("%d",matrix_2[i][j]);
         }
         printf("\n");
     }
@@ -39,22 +77,17 @@ int main()
 {
     char flag[50];
     scanf("%s",flag);
+    int size;
+    scanf("%d",&size);
+    int matrix_1[size][size];
+    int matrix_2[size][size];
     if(!strcmp(flag,"R")){
-        int size;
-    	scanf("%d",&size);
-        int matrix[size][size];
-        //printf("row col %d\n",matrix[0][0]);
-        printf("size: %d\n",size );
-    	random_matrices(size, matrix);
+    	generate_random_matrices(size, matrix_1, matrix_2);
     }else if(!strcmp(flag,"I")){
-    	int size = 0;
-    	scanf("%d",&size);
-    	printf("size: %d\n",size);
-    	int matrix_1[size][size];
-    	matrices(size,matrix_1);
+    	generate_matrices(size,matrix_1, matrix_2);
 
     }else{
-       //printf("Flag else type: %s\n",flag);
+       printf("Invalid flag: %s\n",flag);
     }
 
 	return 0;
