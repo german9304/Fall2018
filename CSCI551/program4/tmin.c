7f172d9ef3e027d8cb0ee95314cc675a443f1c41
german9304 <31298672+german9304@users.noreply.github.com>
2018-10-24 13:06:30 -0700

    comments

--- CSCI551/program4/tmin.c
+++ CSCI551/program4/tmin.c
@@ -2,7 +2,7 @@
 #include <math.h> 
 
 
-double trapMethod(int a, int b, int t);
+double trapMethod(int left_endpt, int right_endpt, int count);
 double f(double x);
 
 
@@ -14,14 +14,32 @@ int main(){
     printf("res: %f\n",res);
     return 0;
 }
-
-double trapMethod(int a, int b, int t){
-	a++;
-	b++;
-	t++;
+/*
+ * Function:     trapMethod
+ * Purpose:      Serial function for estimating a definite integral 
+ *               using the trapezoidal rule
+ * Input args:   left_endpt
+ *               right_endpt
+ *               trap_count 
+ *               base_len
+ * Return val:   Trapezoidal rule estimate of integral from
+ *               left_endpt to right_endpt using trap_count
+ *               trapezoids
+ */
+double trapMethod(int left_endpt, int right_endpt, int count){
+	left_endpt++;
+	right_endpt++;
+	count++;
 	return 0.0;
 }
-
+/*
+ * Function:     f
+ * Purpose:      value of function to be integrated
+ *
+ * Input args:   x
+ *
+ * Return val:   
+ */
 double f(double x){
  double sqrtIn = pow((5 * sin(x / 9)) + (8 * sin(x / 2)), 4);
  return (-3 * (cos(x / 6))) + (sqrt(sqrtIn)) + 1;
