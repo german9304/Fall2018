5638a425315f0d97b6019e90322c53e2a8f1da59
german9304 <31298672+german9304@users.noreply.github.com>
2018-11-13 10:12:49 -0800

    hw8

--- CSCI398/hw8/main.cpp
+++ CSCI398/hw8/main.cpp
@@ -18,64 +18,65 @@ using std::make_pair;
 
 
 /* Standard Binary Search function*/
-int binarySearch(int arr[], int low,  
+int binarySearch(vector<int> av, int low,  
                   int high, int key) 
 { 
   if (high < low) 
     return -1; 
       
   int mid = (low + high)/2; /*low + (high - low)/2;*/
-  if (key == arr[mid]) 
+  if (key == av[mid]) 
     return mid; 
       
-  if (key > arr[mid]) 
-    return binarySearch(arr, (mid + 1), high, key); 
+  if (key > av[mid]) 
+    return binarySearch(av, (mid + 1), high, key); 
       
   // else 
-    return binarySearch(arr, low, (mid -1), key); 
+    return binarySearch(av, low, (mid -1), key); 
 } 
   
 /* Function to get pivot. For array 3, 4, 5, 6, 1, 2 
    it returns 3 (index of 6) */
-int findPivot(int arr[], int low, int high) 
+int findPivot(vector<int> av, int low, int high) 
 { 
   // base cases 
   if (high < low) return -1; 
   if (high == low) return low; 
   
    int mid = (low + high)/2; /*low + (high - low)/2;*/
-   if (mid < high && arr[mid] > arr[mid + 1]) 
+   if (mid < high && av[mid] > av[mid + 1]) 
     return mid; 
       
-   if (mid > low && arr[mid] < arr[mid - 1]) 
+   if (mid > low && av[mid] < av[mid - 1]) 
     return (mid-1); 
       
-   if (arr[low] >= arr[mid]) 
-    return findPivot(arr, low, mid-1); 
+   if (av[low] >= av[mid]) 
+    return findPivot(av, low, mid-1); 
       
-   return findPivot(arr, mid + 1, high); 
+   return findPivot(av, mid + 1, high); 
 } 
   
 /* Searches an element key in a pivoted 
    sorted array arr[] of size n */
-int searchRotated(int arr[], int n, int key) 
+int searchRotated(vector<int> av, int n, int key) 
 { 
-  int pivot = findPivot(arr, 0, n-1); 
+  int pivot = findPivot(av, 0, n-1); 
   
   // If we didn't find a pivot,  
   // then array is not rotated at all 
   if (pivot == -1) 
-    return binarySearch(arr, 0, n-1, key); 
+    return binarySearch(av, 0, n-1, key); 
   
   // If we found a pivot, then first compare with pivot 
   // and then search in two subarrays around pivot 
-  if (arr[pivot] == key) 
+  if (av[pivot] == key) 
     return pivot; 
       
-  if (arr[0] <= key) 
-    return binarySearch(arr, 0, pivot-1, key); 
+  if (av[0] <= key){
+    return binarySearch(av, 0, pivot-1, key);
+    } 
       
-    return binarySearch(arr, pivot+1, n-1, key); 
+    return binarySearch(av, pivot+1, n-1, key); 
 } 
 
 
@@ -168,10 +169,12 @@ int main(){
 				cin >> x;
 				numbers.push_back(x);
 			}//for
-
+      
 			//call your function (or functions) to solve this problem here:
 			//don't forget to output the result
-		      
+    //  cout << "number : " << numbers[0] <<endl;
+		    int res = searchRotated(numbers, N, M);
+        cout << res <<endl;
         }else{
 			cerr << "ERROR: command " << command << " is not recognized." << endl;
 			return 1;
