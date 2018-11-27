71890de426347b757ee0f79c8a6a7f8b96c7d503
german9304 <31298672+german9304@users.noreply.github.com>
2018-11-26 23:03:00 -0800

    program4

--- CSCI551/program4/matrix_mpi.c
+++ CSCI551/program4/matrix_mpi.c
@@ -12,13 +12,22 @@ int main(void){
 
 
 
-    int numbers[10] = {1,2,3,4,5,6,7,8,9,10};
+    int numbers[4] = {5,2,3,4};
+    int remind = 4 % comm_sz;
+    if(my_rank == 0){
+        int s = ((comm_sz - 1) * remind);
+    printf("s:%d\n", 4 - s);
+    }
+    int sendCounts[3] = {1,1,2};
+    int dpls[3] = {0, 1, 2};
     int temp[2];
     
     if(my_rank == 0){
-    	MPI_Scatter(numbers, 2, MPI_INT, temp, 2, MPI_INT, 0, MPI_COMM_WORLD);
+    	MPI_Scatterv(numbers, sendCounts, dpls, MPI_INT, 
+            temp, 2, MPI_INT, 0, MPI_COMM_WORLD);
     }else{
-    	MPI_Scatter(numbers, 2, MPI_INT, temp, 2, MPI_INT, 0, MPI_COMM_WORLD);
+    	MPI_Scatterv(numbers, sendCounts, dpls, MPI_INT, 
+            temp, 2, MPI_INT, 0, MPI_COMM_WORLD);
     }
 
 
@@ -39,3 +48,76 @@ int main(void){
     MPI_Finalize();
 	return 0;
 }
+
+
+
+// #include <mpi.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <math.h>
+
+// #define SIZE 4
+
+// int main(int argc, char *argv[])
+// {
+//     int rank, size;     // for storing this process' rank, and the number of processes
+//     int *sendcounts;    // array describing how many elements to send to each process
+//     int *displs;        // array describing the displacements where each segment begins
+
+//      // elements remaining after division among processes
+//     int sum = 0;                // Sum of counts. Used to calculate displacements
+//     char rec_buf[100];          // buffer where the received data should be stored
+
+//     // the data to be distributed
+//     char data[SIZE][SIZE] = {
+//         {'a', 'b', 'c', 'd'},
+//         {'e', 'f', 'g', 'h'},
+//         {'i', 'j', 'k', 'l'},
+//         {'m', 'n', 'o', 'p'}
+//     };
+
+//     MPI_Init(&argc, &argv);
+//     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+//     MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+//     sendcounts = malloc(sizeof(int)*size);
+//     displs = malloc(sizeof(int)*size);
+
+//     int rem = (SIZE*SIZE)%size;
+
+//     // calculate send counts and displacements
+//     for (int i = 0; i < size; i++) {
+//         sendcounts[i] = (SIZE*SIZE)/size;
+//         if (rem > 0) {
+//             sendcounts[i]++;
+//             rem--;
+//         }
+
+//         displs[i] = sum;
+//         sum += sendcounts[i];
+//     }
+
+//     // print calculated send counts and displacements for each process
+//     if (0 == rank) {
+//         for (int i = 0; i < size; i++) {
+//             printf("sendcounts[%d] = %d\tdispls[%d] = %d\n", i, sendcounts[i], i, displs[i]);
+//         }
+//     }
+
+//     // divide the data among processes as described by sendcounts and displs
+//     MPI_Scatterv(&data, sendcounts, displs, MPI_CHAR, &rec_buf, 100, MPI_CHAR, 0, MPI_COMM_WORLD);
+
+//     // print what each process received
+//     printf("%d: ", rank);
+//     for (int i = 0; i < sendcounts[rank]; i++) {
+//         printf("%c\t", rec_buf[i]);
+//     }
+//     printf("\n");
+
+//     MPI_Finalize();
+
+//     free(sendcounts);
+//     free(displs);
+
+//     return 0;
+// }
